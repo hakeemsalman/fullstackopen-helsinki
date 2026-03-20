2bdceff4e0d62bf9615d6d38f62aa8ca549a386a
hakeemsalman <salman74163@gmail.com>
2026-03-20 15:43:13 +0530

    course-info step 6

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,60 +1,44 @@
+import {useState} from "react"
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
- const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
   }
 
-  const Part = (props) => {
-    return (
-      <p>{props.part} {props.exercise}</p>
-    )
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
   }
 
-  const Header = (props) => {
-    console.log(props)
-    return (
-      <h1>{props.course}</h1>
-    )
-  }
-
-  const Content = (props) => {
-    return (
-      <>
-        <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
-        <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
-        <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
-      </>
-    )
-  }
-
-  const Total = (props) => {
-    let sum = 0
-    const temp = props.parts.forEach(n => {
-      sum+=n.exercises
-    })
-    return (
-      <p>Number of exercises {sum}</p>
-    )
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   )
 }
